47677c613a02f55479692d7f24ed5f9050d403d4
lgnbhl <felix.luginbuhl@protonmail.ch>
2021-02-23 21:13:59 +0100

    scroller 0.1.1. Improve dependencies.

---
+++ NEWS.md
@@ -0,0 +1,5 @@
+# scroller 0.1.1
+
+* Added a `NEWS.md` file to track changes to the package.
+* create function html_dependencies_scroller().
+* Improve HTML dependencies. Now using DNS by default. If not working, use local files.
--- R/use_scroller.R
+++ R/use_scroller.R
@@ -3,6 +3,7 @@
 #' @param animationLength integrer. Lenght of the animation
 #' @param easingFunction string. An effect from \href{http://gsgd.co.uk/sandbox/jquery/easing/}{jquery-easing}.
 #' @param scrollOffset integrer. scrollOffset
+#' @param use_cdn use CDN (by default) or use locally stored files.
 #' 
 #' @importFrom jsonlite toJSON
 #' @importFrom htmltools tags tagList
@@ -12,7 +13,7 @@
 #' use_scroller()
 #' 
 #' @export
-use_scroller <- function(animationLength = 750, easingFunction = "linear", scrollOffset = 0){
+use_scroller <- function(animationLength = 750, easingFunction = "linear", scrollOffset = 0, use_cdn = TRUE){
 
   options <- paste0("$(document).ready(function(){
                       AA_CONFIG = {animationLength: ", animationLength,", easingFunction: '", easingFunction,"', scrollOffset:", scrollOffset,"};
@@ -21,32 +22,50 @@ use_scroller <- function(animationLength = 750, easingFunction = "linear", scrol
   class(options) <- "json"
   
   tagList(
-    html_dependencies_scroller(),
+    html_dependencies_scroller(use_cdn = use_cdn),
     htmltools::tags$script(options)
   )
 }
 
-html_dependencies_scroller <- function() {
-  list(
-    htmltools::htmlDependency(
-      name = "jquery.arbitrary-anchor",
-      version = "1.0",
-      package = "scroller",
-      src = c(
-        file = "",
-        url = ""
-      ),
-    script = "htmlwidgets/jquery.arbitrary-anchor.js-1.0/jquery.arbitrary-anchor.js"
-    ),
-    htmltools::htmlDependency(
-      name = "jquery.easing",
-      version = "1.4.1",
-      package = "scroller",
-      src = c(
-        file = "",
-        url = ""
-      ),
-      script = "htmlwidgets/jquery.easing-1.4.1/jquery.easing.min.js"
-    )
+#' scroller dependencies
+#' 
+#' Get scroller html dependencies.
+#' 
+#' @param use_cdn use CDN (by default) or use locally stored files.
+#' 
+#' @importFrom htmltools tagList htmlDependency
+#' 
+#' @rdname scroller-dependencies
+#' 
+#' @export
+
+html_dependencies_scroller <- function(use_cdn = TRUE) {
+  
+  # jquery.arbitrary-anchor.js
+  jqueryarbitraryanchor <- htmltools::htmlDependency(
+    name = "jqueryarbitraryanchor",
+    version = "2.0.0",
+    src = "",
+    script = "assets/jquery.arbitrary-anchor.js",
+    package = "scroller"
   )
+  
+  # jquery.easing.min.js 1.4.1
+  if(use_cdn)
+    jqueryeasing <- htmltools::htmlDependency(
+      name = "jqueryeasing",
+      version = "1.4.1",
+      src = c(href = "https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/"),
+      stylesheet = "jquery.easing.min.js"
+    )
+  else 
+    jqueryeasing <- htmltools::htmlDependency(
+      "jqueryeasing",
+      "1.4.1",
+      src = "",
+      stylesheet = "assets/jquery.easing.min.js",
+      package = "scroller"
+    )
+  
+  htmltools::tagList(jqueryeasing, jqueryarbitraryanchor)
 }
